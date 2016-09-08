7df26fdd0ce77e08b3bf11afe10b9243346b206f
tin <tingeber@gmail.com>
2016-09-08 14:35:32 +0200

    content add

--- views/homepage.php
+++ views/homepage.php
@@ -1,13 +1,14 @@
 <div class="row center-align HomepageBlocks">
     <div class="col s12 l4 HomepageBlock is-namibia">
-        <a href="http://namibia.diig.io" class="HomepageBlock-inner">
+        <a href="http://transparentoil.org" class="HomepageBlock-inner">
             <div class="HomepageBlock-innerBg">
                 <p class="Header-title">
                     <span class="Header-title--secondary">Transparent Oil</span>
                     <span class="Header-title--primary">Namibia</span>
                 </p>
                 <p class="HomepageBlock-desc">
-                    Gender humanitarian relief life-saving Medecins du Monde; partner NGO philanthropy marginalized communities. Organization transformative Rosa Parks, developing nations tackle.
+                  Explore the most complete data set of petroleum exploration licenses in Namibia and information about management structures for the companies behind these licenses. You can search by location, company name, and license information.
+                  <br />  &nbsp;
                 </p>
             </div>
             <div class="HomepageBlock-logo">
@@ -20,11 +21,14 @@
         <a href="http://malawi.diig.io" class="HomepageBlock-inner">
             <div class="HomepageBlock-innerBg">
                 <p class="Header-title">
-                    <span class="Header-title--secondary">Transparent mining</span>
+                    <span class="Header-title--secondary">Migodi Yathu</span>
                     <span class="Header-title--primary">Malawi</span>
                 </p>
                 <p class="HomepageBlock-desc">
-                    Gender humanitarian relief life-saving Medecins du Monde; partner NGO philanthropy marginalized communities. Organization transformative Rosa Parks, developing nations tackle.
+                    Explore the most complete set of data on minerals in Malawi and information about companies involved in exploring and extracting the country’s minerals.
+                    <br />You can search by company, mineral and oil licences.
+                    <br />  &nbsp;
+                    <br />  &nbsp;
                 </p>
             </div>
             <div class="HomepageBlock-logo">
@@ -37,11 +41,11 @@
         <a href="http://zimbabwe.diig.io" class="HomepageBlock-inner">
             <div class="HomepageBlock-innerBg">
                 <p class="Header-title">
-                    <span class="Header-title--secondary">Transparent Mining</span>
+                    <span class="Header-title--secondary">Mining Pachena</span>
                     <span class="Header-title--primary">Zimbabwe</span>
                 </p>
                 <p class="HomepageBlock-desc">
-                    Gender humanitarian relief life-saving Medecins du Monde; partner NGO philanthropy marginalized communities. Organization transformative Rosa Parks, developing nations tackle.
+                    Explore the most complete set of data on mining in Zimbabwe, information about company hierarchies and management structures of mining companies, community monitoring reports on mining impacts and mineral revenue payments to the Zimbabwe government. You can search by company, mine and mineral types.
                 </p>
             </div>
             <div class="HomepageBlock-logo">
